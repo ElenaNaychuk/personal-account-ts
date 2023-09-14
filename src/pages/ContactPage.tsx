1ef00f13c41a9293b40c716c26d4c3424524428c
Elena Naychuk <106145336+ElenaNaychuk@users.noreply.github.com>
2023-09-14 06:59:20 +0300

    create pages, routing

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ContactPage:React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+export {ContactPage};
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ErrorPage:React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+export {ErrorPage};
